4eca01826188d0f772dfc4640d2d90451e6c3fe6
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2023-03-08 12:59:27 +0200

    javascript-tut-1 7-relational-operators

---
+++ javascript-tut-1/7-relational-operators.js
@@ -0,0 +1,20 @@
+let x = 6;
+let y = 6;
+
+let data = x >= y;
+
+console.log(data);
+
+x = "3";
+y = 3;
+
+data = x === y; ///use trible === and not double(==) equal sign
+
+console.log(data);
+
+/*
+ | Operators | Data | Type | 
+_|___________|______|______|
+_|___(==)____|checks|   X  |
+_|___(===)___|checks|checks|
+*/
