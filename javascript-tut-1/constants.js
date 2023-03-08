044c839f453fd4feaed5df3031e172ef09c28d20
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2023-03-08 09:43:42 +0200

    javascript-tut-1 constant variables

---
+++ javascript-tut-1/constants.js
@@ -0,0 +1,8 @@
+let radius = 5; //normal variable
+const pi = 3.14; //constant variable. (give a constant key to a value that  can not be changed like pi)
+let area;
+
+radius = 6;
+area = pi * radius * radius;
+
+console.log(area);
