0194126dc26befcab4081ca036187425c656216b
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2023-03-08 22:18:38 +0200

    javascript-tut-1 16-objects

---
+++ javascript-tut-1/16-objects.js
@@ -0,0 +1,14 @@
+let alien = {}; //object
+
+alien = {
+  name: "Katlego",
+  tech: "JS",
+  age: 23,
+};
+
+console.log(alien.age); /**use . or [] */
+console.log(alien["name"]);
+
+let input = "tech";
+
+console.log(alien[input]);
