a7293329030bf7e2bda4fecd3e6ff5141d2e0f89
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2023-03-08 16:24:59 +0200

    javascript-tut-1 10-ternary-operator

---
+++ 10-ternary-operator.js
@@ -0,0 +1,11 @@
+let num = 8;
+let result;
+
+// if (num % 2 === 0) result = "even";
+// else result = "odd";
+
+//using ternary operator (?:)
+
+result = num % 2 === 0 ? "even" : "odd"; //if the statement is true value after ? will be executed and if its false the value after : will be executed
+
+console.log(result);
