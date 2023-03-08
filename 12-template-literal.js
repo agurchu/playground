7b926a420ecfb319edfe24cd31c02d4fa2cb1138
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2023-03-08 16:59:03 +0200

    javascript-tut-1 12-template-literal

---
+++ 12-template-literal.js
@@ -0,0 +1,10 @@
+let num1 = 9;
+let num2 = 4;
+let result = num1 + num2;
+
+console.log(`The addition of ${num1} and ${num2} is ${result}`); // ${} to highlight the values of the variables
+
+//Or use console.log("The addition of " + num1 + " and " + num2 + " is " + result);
+
+console.log(`My Name is
+Katlego`); //instead of using \n
