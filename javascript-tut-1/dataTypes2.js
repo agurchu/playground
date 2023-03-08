6b24f83a61489498ac623235eb3e88b06ed538c4
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2023-03-08 10:45:32 +0200

    javascript-tut-1 data types 2

---
+++ javascript-tut-1/dataTypes2.js
@@ -0,0 +1,30 @@
+//______________________Strings
+
+let firstName = "Katlego";
+let lastName = "Mtimane";
+
+let user = firstName + " " + lastName; // use " " to create space between
+
+console.log(user);
+
+user = 'Katlego Mtimane "M-ritmo"'; // forward slash to cancel the computer meaning of ""
+console.log(user); // out put Katlego Mtimane "M-ritmo"
+
+user = "Katlego \nMtimane"; // \n means new line
+console.log(user);
+
+user = "Ka\tlego"; // the \ between letters creates a tab
+
+console.log(user);
+
+user = "Ka\vtlego";
+console.log(user);
+
+//__________________boolean
+
+let bool = 5 > 6;
+console.log(bool); // false
+
+//__________________________null
+
+//it represent something that is empty or does not exist and also an objectS
