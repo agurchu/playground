286e3790ad626bf36cd1adb0b5c6a6e82d323f7f
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2023-03-08 09:33:25 +0200

    javascript-tut-1 variables

---
+++ javascript-tut-1/variables.js
@@ -0,0 +1,24 @@
+let num = 4;
+
+console.log(num);
+
+num = 9; //not use the key word 'let' again because 'num' has already been defined  on the first line
+
+console.log(num);
+
+let username = "katlego"; //always use double codes "" when assigning a text/string
+let user = "katlego's car"; //always use double codes "" when assigning a text/string
+let user_name = "katlego"; //snake casing rule
+let userName = "katlego"; //camel casing rule
+
+console.log(username);
+
+console.log(user);
+
+//variable it is something that can change.
+
+/* variable name
+>can have characters
+>can have numbers except first position(1username)
+>can use $ and _ symbol
+*/
