752155b17e9c1408c8f462cfc6872d40dcbf9e8f
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2023-03-10 19:41:27 +0200

    object-oriented constructor function

--- object-oriented/index.js
+++ object-oriented/index.js
@@ -11,3 +11,15 @@ function createCircle(radius) {
 
 const circle = createCircle(1);
 circle.draw();
+
+//constructor function (its name starts with a cap letter)
+
+function Circle(radius) {
+  this.radius = radius;
+  this.draw = function () {
+    console.log("draw");
+  };
+}
+
+const anotherCircle = new Circle(1);
+console.log(anotherCircle);
