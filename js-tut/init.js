605233374e74bb937eda95a6890347438ffdcd5d
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2023-03-11 10:22:37 +0200

    js-tut inputs

---
+++ js-tut/init.js
@@ -0,0 +1,10 @@
+//operators
+// const fruit = prompt("what is your fav fruit?");
+
+const food = Number(prompt("how much was the food"));
+const tipPercentage = Number(prompt("tip %?") / 100);
+const tipAmount = food * tipPercentage;
+const total = food + tipAmount;
+console.log(total);
+
+//user input
