eca412f191c422d4da49efadf3d09ab2e2cc2225
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2023-03-11 20:59:58 +0200

    js-tut for-loop

--- js-tut/for-loop.js
+++ js-tut/for-loop.js
@@ -1,17 +1,3 @@
-/**Math Operators
- * Multiply *
- * Divide /
- * Exponent **
- * Modulo/reimander % (5%2=1)
- */
-
-/**MAth Methods
- 
- *Floor - rounds down (5.7 => 5) Math.floor(5.7)
- *ceil - round up (5.7 => 6) Math.ceil(5.7)
- *Random - gives a random number btween 0 & 1 (Math.random() => 0.2255636)
- */
-
 // for loops
 //array
 const fruits = [
