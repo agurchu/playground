292a394aacb4552aa7c08b476274d04eedcf1ecb
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2023-03-11 11:01:45 +0200

    js-tut Baby weather app

--- js-tut/init.js
+++ js-tut/init.js
@@ -1,10 +1,35 @@
 //operators
 // const fruit = prompt("what is your fav fruit?");
-
-const food = Number(prompt("how much was the food"));
-const tipPercentage = Number(prompt("tip %?") / 100);
-const tipAmount = food * tipPercentage;
-const total = food + tipAmount;
-console.log(total);
-
 //user input
+
+// const food = Number(prompt("how much was the food"));
+// const tipPercentage = Number(prompt("tip %?") / 100);
+// const tipAmount = food * tipPercentage;
+// const total = food + tipAmount;
+// console.log(total);
+
+/**Math Operators
+ * Multiply *
+ * Divide /
+ * Exponent **
+ * Modulo/reimander % (5%2=1)
+ */
+
+/**MAth Methods
+ 
+ *Floor - rounds down (5.7 => 5) Math.floor(5.7)
+ *ceil - round up (5.7 => 6) Math.ceil(5.7)
+ *Random - gives a random number btween 0 & 1 (Math.random() => 0.2255636)
+ */
+
+// Baby weather app (conditionals)
+// if rain 'grab your umbrella'
+// else 'wear sunglasses'
+
+const weather = prompt("How is the weather?");
+
+if (weather == "rainy") {
+  alert("grab your umbrella");
+} else {
+  alert("wear sunglasses");
+}
