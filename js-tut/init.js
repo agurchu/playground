1510730e5913a5c5149a43dfb0e69ba0bc3dc825
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2023-03-13 20:10:09 +0200

    js-tut changing divs to button

--- js-tut/init.js
+++ js-tut/init.js
@@ -11,3 +11,28 @@
  *ceil - round up (5.7 => 6) Math.ceil(5.7)
  *Random - gives a random number btween 0 & 1 (Math.random() => 0.2255636)
  */
+
+// DOM Manipulation (document object model)
+
+let titleDev = document.getElementById("title");
+
+let msg = "Goodbye my lover";
+
+titleDev.innerText = msg;
+
+console.log(titleDev.innerText);
+
+titleDev.innerHTML = `<p>${msg}</p>`;
+
+titleDev.style.background = "lightblue";
+
+let redDiv = document.getElementById("red");
+let yellowDiv = document.getElementById("yellow");
+let greenDiv = document.getElementById("green");
+let container = document.querySelectorAll(".container button");
+
+container.forEach((div) => (div.style.cursor = "pointer"));
+
+redDiv.onclick = () => console.log("you clicked red");
+yellowDiv.onclick = () => console.log("you clicked yellow");
+greenDiv.onclick = () => console.log("you clicked green");
