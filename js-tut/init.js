0e66debc51aeea94bff7328a70daf5b343dccd01
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2023-03-14 13:48:13 +0200

    js tut if else if else

--- js-tut/init.js
+++ js-tut/init.js
@@ -11,3 +11,29 @@
  *ceil - round up (5.7 => 6) Math.ceil(5.7)
  *Random - gives a random number btween 0 & 1 (Math.random() => 0.2255636)
  */
+
+const randomFruit = (fruits) => {
+  const randomNumber = Math.floor(Math.random() * fruits.length);
+  return fruits[randomNumber];
+};
+const fruits = ["banna", "apple", "pear", "orange"];
+// console.log(randomFruit(fruits));
+
+// if else if else
+// rainy(1), overcast (0), sunny(-1)
+
+const scores = [1, 0, -1];
+
+const weatherScore = (weather) => {
+  let score;
+  if (weather === "rainy") {
+    score = 1;
+  } else if (weather === "overcast") {
+    score = 0;
+  } else {
+    score = -1;
+  }
+  return score;
+};
+
+console.log(weatherScore("rainy"));
