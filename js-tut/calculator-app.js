8925ca97e75ac00b36dc528eed3b3173c8a01069
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2023-03-14 13:01:55 +0200

    js tut complete calculator

---
+++ js-tut/calculator-app.js
@@ -0,0 +1,72 @@
+/* App: Calulator
+
+These are  the 3 functions to use
+=================================
+calculateBill()
+incresePeople()
+decresePeople()
+
+All div ID's to access
+======================
+#1 ID "billTotalInput" = User input for bill total
+#2 ID "tipInput" = user input for tip
+#3 ID "numberOfPeople" = Current number of people that are splited
+#4 ID "perPersonTotal" = Total dollar value owed per person
+*/
+
+const billInput = document.getElementById("billTotalInput");
+const tipInput = document.getElementById("tipInput");
+const numOfPeopleDiv = document.getElementById("numberOfPeople");
+const perPersonTotal = document.getElementById("perPersonTotal");
+
+// get number of people from number of people
+let numberOfPeople = Number(numOfPeopleDiv.innerText);
+
+// ** Calculate the total bill per person
+
+const calculateBill = () => {
+  // get bill from user input & convert it into a number
+  const bill = Number(billInput.value);
+
+  // get the tip from user & convert it into a percentage
+  const tipPercentage = Number(tipInput.value) / 100;
+
+  // get the total tip amount
+
+  const tipAmount = bill * tipPercentage;
+
+  // calculate the total (tip amount + bill)
+
+  const total = tipAmount + bill;
+
+  // calculate total of per person ( total / number of perple)
+  const totalPerPerson = total / numberOfPeople;
+  perPersonTotal.innerText = `R ${totalPerPerson.toFixed(2)}`;
+};
+
+// ** splits the bill per person
+const incresePeople = () => {
+  // increment
+  numberOfPeople++;
+
+  numOfPeopleDiv.innerText = numberOfPeople;
+
+  calculateBill();
+};
+
+const decresePeople = () => {
+  // if (numberOfPeople > 1) {
+  //   // decrement
+  //   numberOfPeople--;
+  //   numOfPeopleDiv.innerText = numberOfPeople;
+  // }
+
+  if (numberOfPeople <= 1) {
+    alert("number of people can not be less than 1");
+    return;
+  }
+
+  numberOfPeople--;
+  numOfPeopleDiv.innerText = numberOfPeople;
+  calculateBill();
+};
