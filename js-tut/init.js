7e36af16510ffb88f411f4107f0041b8754f81f5
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2023-03-14 10:48:44 +0200

    js tut calculator global variables

--- js-tut/init.js
+++ js-tut/init.js
@@ -11,3 +11,24 @@
  *ceil - round up (5.7 => 6) Math.ceil(5.7)
  *Random - gives a random number btween 0 & 1 (Math.random() => 0.2255636)
  */
+
+/* App: Calulator
+
+These are  the 3 functions to use
+=================================
+calculateBill()
+incresePeople()
+decresePeople()
+
+All div ID's to access
+======================
+#1 ID "billTotalInput" = User input for bill total
+#2 ID "tipInput" = user input for tip
+#3 ID "numberOfPeople" = Current number of people that are splited
+#4 ID "perPersonTotal" = Total dollar value owed per person
+*/
+
+const billInput = document.getElementById("billTotalInput");
+const tipInput = document.getElementById("tipInput");
+const numOfPeople = document.getElementById("numberOfPeople");
+const perPersonTotal = document.getElementById("perPersonTotal");
