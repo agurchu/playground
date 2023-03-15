ec5ecde030d97ba464cdd56a44c65205a1794d39
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2023-03-15 19:24:52 +0200

    js tut weather fetch

--- js-tut/init.js
+++ js-tut/init.js
@@ -11,3 +11,18 @@
  *ceil - round up (5.7 => 6) Math.ceil(5.7)
  *Random - gives a random number btween 0 & 1 (Math.random() => 0.2255636)
  */
+
+// ** weather app **
+
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "64b26c83ffmshe693692fbe4eb3cp1fdedcjsn071ec3a22fb3",
+    "X-RapidAPI-Host": "open-weather13.p.rapidapi.com",
+  },
+};
+
+fetch("https://open-weather13.p.rapidapi.com/city/pretoria", options)
+  .then((response) => response.json())
+  .then((data) => console.log(data))
+  .catch((err) => console.error(err));
