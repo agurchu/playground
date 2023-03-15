2fa6bc8c0671981dea8760c6352049590b5e1d04
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2023-03-15 09:05:14 +0200

    js tut super hero api prac

--- js-tut/init.js
+++ js-tut/init.js
@@ -11,3 +11,25 @@
  *ceil - round up (5.7 => 6) Math.ceil(5.7)
  *Random - gives a random number btween 0 & 1 (Math.random() => 0.2255636)
  */
+
+// ** superhero api
+
+const superHero_Token = "3472708443009585";
+const base__url = `https://superheroapi.com/api.php/${superHero_Token}`;
+
+const getSuperHero = (id) => {
+  const hero = document.getElementById("hero");
+
+  fetch(`${base__url}/${id}`)
+    .then((response) => response.json())
+    .then((json) => {
+      console.log(json);
+      hero.innerHTML += `<img src="${json.image.url}" style="width: 200px;"/>`;
+      const h2EL = document.createElement("h2");
+      h2EL.innerText = json.name;
+
+      hero.appendChild(h2EL);
+    });
+};
+
+getSuperHero(22);
