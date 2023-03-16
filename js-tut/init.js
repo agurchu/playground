d107698c90cabc050aa2fac7c1089c735a5ea657
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2023-03-16 20:10:48 +0200

    js tut classes intro

--- js-tut/init.js
+++ js-tut/init.js
@@ -11,3 +11,26 @@
  *ceil - round up (5.7 => 6) Math.ceil(5.7)
  *Random - gives a random number btween 0 & 1 (Math.random() => 0.2255636)
  */
+
+// ** Classes (methods & properties)
+
+class Car {
+  constructor(name, color, speed) {
+    this.name = name; // properties have "this"
+    this.color = color; // properties have "this"
+    this.speed = speed; // properties have "this"
+  }
+  //methods. anything with () Under object
+  drive() {
+    console.log("drove 2 miles");
+  }
+  //methods. anything with () Under object
+  brake() {
+    console.log("came from street");
+  }
+}
+
+const ferrari = new Car("ferrari", "red", 250);
+ferrari.drive();
+console.log(ferrari.color);
+ferrari.brake();
