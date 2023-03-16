9d4c11b36d3a93a5da019c881f6ce90b759e01e4
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2023-03-16 11:07:51 +0200

    replacing weather api

--- js-tut/init.js
+++ js-tut/init.js
@@ -14,15 +14,53 @@
 
 // ** weather app **
 
+// const options = {
+//   method: "GET",
+//   headers: {
+//     "X-RapidAPI-Key": "64b26c83ffmshe693692fbe4eb3cp1fdedcjsn071ec3a22fb3",
+//     "X-RapidAPI-Host": "open-weather13.p.rapidapi.com",
+//   },
+// };
+
+/**things to fetch using api-ninja weather
+ * min_temp
+
+ * temp
+
+ * max_temp
+feels_like
+
+
+ */
+const apiNinjas_key = "+V1mjbWMB9QlX5LMv9V/BA==RTc898ZhuOvN2tlC";
+const cityInput = document.getElementById("city-input");
+const cityName = document.getElementById("city-name");
+const weatherType = document.getElementById("weather-type");
+const tempEl = document.getElementById("temp");
+const minTempEl = document.getElementById("min-temp");
+const maxTempEl = document.getElementById("max-temp");
+
 const options = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": "64b26c83ffmshe693692fbe4eb3cp1fdedcjsn071ec3a22fb3",
-    "X-RapidAPI-Host": "open-weather13.p.rapidapi.com",
+    "X-Api-Key": apiNinjas_key,
   },
+  contentType: "application/json",
 };
-
-fetch("https://open-weather13.p.rapidapi.com/city/pretoria", options)
+const fetchWeather = fetch(
+  `https://api.api-ninjas.com/v1/weather?city=${cityName}`,
+  options
+)
   .then((response) => response.json())
   .then((data) => console.log(data))
-  .catch((err) => console.error(err));
+  .catch((err) => console.error("Error: ", err));
+
+console.log(fetchWeather);
+
+const searchCity = (city, temp, mintemp, maxtemp) => {
+  cityName.innerText = cityInput.value;
+  city = cityInput.value;
+  tempEl.innerText = temp;
+  minTempEl.innerText = mintemp;
+  maxTempEl.innerText = maxtemp;
+};
