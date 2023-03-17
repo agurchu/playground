4b2c691ddf52836f959117833f2bffd084e6259a
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2023-03-17 20:44:06 +0200

    js tut setting simulate btn

--- js-tut/init.js
+++ js-tut/init.js
@@ -11,3 +11,14 @@
  *ceil - round up (5.7 => 6) Math.ceil(5.7)
  *Random - gives a random number btween 0 & 1 (Math.random() => 0.2255636)
  */
+
+// ** Fight game
+
+// grabs elements from DOM
+
+const playBtn = document.getElementById("play");
+const resultEl = document.getElementById("result");
+const p1Name = document.getElementById("p1__name");
+const p2Name = document.getElementById("p2__name");
+const p1HealthEl = document.getElementById("p1__heal");
+const p2HealthEl = document.getElementById("p1__heal");
