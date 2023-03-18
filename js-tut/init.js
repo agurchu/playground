16da8e8aae390c6b2ba410259104c6a99c36e389
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2023-03-18 21:22:55 +0200

    js tut netflix css styling up to original movies

--- js-tut/init.js
+++ js-tut/init.js
@@ -64,4 +64,42 @@ const showMovies = (movies, dom_element, path_type) => {
   }
 };
 
-fetchMovies(url), ".original__movies", "poster_path";
+fetchMovies(url, ".original__movies", "poster_path");
+// ** Function that fetches Netflix Originals **
+// function getOriginals() {
+
+// }
+// // ** Function that fetches Trending Movies **
+// function getTrendingNow() {
+
+// }
+// // ** Function that fetches Top Rated Movies **
+// function getTopRated() {
+
+// }
+
+// // ** BONUS **
+
+// // ** Fetches URL provided and returns response.json()
+// async function getMovieTrailer(id) {
+//   //URL: `https://api.themoviedb.org/3/movie/${id}/videos?api_key=19f84e11932abbc79e6d83f82d6d1045&language=en-US`
+
+// }
+
+// // ** Function that adds movie data to the DOM
+// const setTrailer = trailers => {
+//   // Set up iframe variable to hold id of the movieTrailer Element
+//   const iframe
+//   // Set up variable to select .movieNotFound element
+//   const movieNotFound
+
+//   // If there is a trailer add the src for it
+//   if (trailers.length > 0) {
+//     // add d-none class to movieNotFound and remove it from iframe
+
+//     // add youtube link with trailers key to iframe.src
+//   } else {
+//     // Else remove d-none class to movieNotfound and ADD it to iframe
+
+//   }
+// }
