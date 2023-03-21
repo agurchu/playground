2d2b67721d0317a0e782b13ca0bccf3116329c65
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2023-03-21 19:24:01 +0200

    api projects news api fixed

--- api-projects/index.js
+++ api-projects/index.js
@@ -1,12 +1,72 @@
+const container = document.querySelector(".container"),
+  optionsContainer = document.querySelector(".container__options");
+
 const apiKey = "d1c724c4899f4e4fba3751776b75a8af";
 
-url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`;
-
-const getNews = () => {
-  const req = new Request(url);
-  fetch(req).then((response) => {
-    console.log(response.json());
-  });
+window.onload = () => {
+  requestURL = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`;
 };
 
-getNews();
+const country = "za";
+const options = [
+  "general",
+  "entertainment",
+  "health",
+  "science",
+  "sports",
+  "technology",
+];
+
+// // 1,000 requests per day
+// let requestURL;
+
+const init = () => {
+  optionsContainer.innerHTML = "";
+  getNews();
+  createOptions();
+};
+
+// create cards from data
+const generateUI = (articles) => {
+  for (let item of articles) {
+    let card = document.createElement("div");
+    card.classList.add("news__card");
+    card.innerHTML = `<div
+    class="news__image-container">
+    <img src="${item.urlToImage || "./newspaper.jpg"}" alt="" />
+    </div>
+
+    <div class="news__content">
+        <div class="news__title">
+        ${item.title}
+        </div>
+
+        <div class="news__description">
+        ${item.description || item.content || ""}
+        </div>
+
+        <a href="${item.url}" target="_blank" class="view__button">Read More</a>
+    </div>`;
+
+    container.appendChild(card);
+  }
+};
+
+// News API Call
+
+const getNews = async () => {
+  container.innerHTML = "";
+  let response = await fetch(
+    `https://newsapi.org/v2/top-headlines?country=${country}&apiKey=${apiKey}`
+  );
+  if (!response.ok) {
+    alert("Data unavailable at the moment. Please try again later");
+    return false;
+  }
+
+  let data = await response.json();
+  console.log(data.articles);
+  generateUI(data.articles);
+};
+
+init();
