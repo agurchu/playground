802ebb48d747f67afa232ce8b3acd27d117b94dd
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2023-03-22 19:36:45 +0200

    react tut kat-blog dynamic values

--- react-tut/kat-blog/src/App.js
+++ react-tut/kat-blog/src/App.js
@@ -1,10 +1,28 @@
 import "./App.css";
 
 function App() {
+  const text = "Welcome to kat blog";
+  const likes = 50;
+  const person = { name: "jim", age: 30 };
+  const fruts = ["banna ", "pear ", "apple "];
+  const link = "https://www.google.com";
+
+  fruts.push("mango ");
   return (
     <div className="App">
       <div className="content">
-        <h1>App Compponent</h1>
+        <h1>{text}</h1>
+        <p>Liked {likes} times </p>
+
+        <p>
+          {person.name} {person.age}
+        </p>
+        <p>{fruts}</p>
+        <p>{Math.ceil(Math.random() * 10)}</p>
+
+        <a href={link} target="_blank">
+          Google site
+        </a>
       </div>
     </div>
   );
