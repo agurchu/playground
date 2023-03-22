cb3a8286ce8a8fdfbb1bc3fccbe7e484655165de
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2023-03-22 21:01:47 +0200

    react tut kat-blog useState hook

--- react-tut/kat-blog/src/Home.js
+++ react-tut/kat-blog/src/Home.js
@@ -1,19 +1,23 @@
-const Home = () => {
-  const handleClick = (e) => {
-    console.log("Hello world", e.target);
-  };
+import { useState } from "react";
 
-  const handleClickAgain = (name) => {
-    console.log(`Hello ${name}`);
+const Home = () => {
+  //   let name = "Jim";
+
+  const [name, setName] = useState("jim");
+  const [age, setAge] = useState(25);
+  const handleClick = () => {
+    setName("croc");
+    setAge(30);
   };
 
   return (
     <div className="home">
       <h2>Homepage</h2>
+      <p>
+        {name} is {age} years old
+      </p>
+
       <button onClick={handleClick}>Click me</button>
-      <button onClick={(e) => handleClickAgain("kat", e)}>
-        Click me again
-      </button>
     </div>
   );
 };
