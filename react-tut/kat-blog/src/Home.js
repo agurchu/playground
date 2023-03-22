e9745128d0572f609e0b14c133e2a920dbc6a12f
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2023-03-22 20:42:39 +0200

    react tut kat-blog click events and functions

--- react-tut/kat-blog/src/Home.js
+++ react-tut/kat-blog/src/Home.js
@@ -1,7 +1,19 @@
 const Home = () => {
+  const handleClick = (e) => {
+    console.log("Hello world", e.target);
+  };
+
+  const handleClickAgain = (name) => {
+    console.log(`Hello ${name}`);
+  };
+
   return (
     <div className="home">
       <h2>Homepage</h2>
+      <button onClick={handleClick}>Click me</button>
+      <button onClick={(e) => handleClickAgain("kat", e)}>
+        Click me again
+      </button>
     </div>
   );
 };
