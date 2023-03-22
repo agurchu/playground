31b247bef8a73c66c5066b35680c1531c0bfd204
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2023-03-22 19:52:25 +0200

    react tut kat-blog multiple vomponents

--- react-tut/kat-blog/src/App.js
+++ react-tut/kat-blog/src/App.js
@@ -1,28 +1,14 @@
 import "./App.css";
+import Navbar from "./Navbar";
+import Home from "./Home";
 
 function App() {
-  const text = "Welcome to kat blog";
-  const likes = 50;
-  const person = { name: "jim", age: 30 };
-  const fruts = ["banna ", "pear ", "apple "];
-  const link = "https://www.google.com";
-
-  fruts.push("mango ");
   return (
     <div className="App">
+      <Navbar />
+
       <div className="content">
-        <h1>{text}</h1>
-        <p>Liked {likes} times </p>
-
-        <p>
-          {person.name} {person.age}
-        </p>
-        <p>{fruts}</p>
-        <p>{Math.ceil(Math.random() * 10)}</p>
-
-        <a href={link} target="_blank">
-          Google site
-        </a>
+        <Home />
       </div>
     </div>
   );
---
+++ react-tut/kat-blog/src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h1>The Kat Blog</h1>
+      <div className="liks">
+        <a href="/">Home</a>
+        <a href="/create">New Blog</a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
