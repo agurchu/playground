3bbe3f4e194631ea47991da14b9bd22858c72e00
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2023-03-23 19:18:50 +0200

    react tut kat-blog controlled inputs

--- react-tut/kat-blog/src/Create.js
+++ react-tut/kat-blog/src/Create.js
@@ -1,7 +1,38 @@
+import { useState } from "react";
+
 const Create = () => {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("kat");
+
   return (
     <div className="create">
       <h2>Add a New Blog</h2>
+      <form>
+        <label>Blog title:</label>
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label>Blog body:</label>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
+        <label>Blog author:</label>
+        <select value={author} onChange={(e) => setAuthor(e.target.value)}>
+          <option value="kat">Kat</option>
+          <option value="jimmy">Jimmy</option>
+        </select>
+        <button>Add Blog</button>
+      </form>
+
+      <p>{title}</p>
+      <p>{body}</p>
+      <p>{author}</p>
     </div>
   );
 };
