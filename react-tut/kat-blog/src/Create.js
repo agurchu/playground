158a9f315adf75c3eeb82004acae188d06d36856
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2023-03-23 19:35:04 +0200

    react tut kat-blog submitting a form

--- react-tut/kat-blog/src/Create.js
+++ react-tut/kat-blog/src/Create.js
@@ -4,35 +4,39 @@ const Create = () => {
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
   const [author, setAuthor] = useState("kat");
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const blog = { title, body, author };
+    console.log(blog);
+  };
 
   return (
     <div className="create">
       <h2>Add a New Blog</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>Blog title:</label>
         <input
           type="text"
           required
           value={title}
-          onChange={(e) => setTitle(e.target.value)}
+          onChange={(event) => setTitle(event.target.value)}
         />
         <label>Blog body:</label>
         <textarea
           required
           value={body}
-          onChange={(e) => setBody(e.target.value)}
+          onChange={(event) => setBody(event.target.value)}
         ></textarea>
         <label>Blog author:</label>
-        <select value={author} onChange={(e) => setAuthor(e.target.value)}>
+        <select
+          value={author}
+          onChange={(event) => setAuthor(event.target.value)}
+        >
           <option value="kat">Kat</option>
           <option value="jimmy">Jimmy</option>
         </select>
         <button>Add Blog</button>
       </form>
-
-      <p>{title}</p>
-      <p>{body}</p>
-      <p>{author}</p>
     </div>
   );
 };
