fb77f1ba2ce032f53ff5859ff8e891360ec442ef
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2023-03-23 08:31:55 +0200

    react tut kat-blog outputting lists

--- react-tut/kat-blog/src/Home.js
+++ react-tut/kat-blog/src/Home.js
@@ -1,23 +1,26 @@
 import { useState } from "react";
 
 const Home = () => {
-  //   let name = "Jim";
-
-  const [name, setName] = useState("jim");
-  const [age, setAge] = useState(25);
-  const handleClick = () => {
-    setName("croc");
-    setAge(30);
-  };
+  const [blogs, setBlogs] = useState([
+    { title: "My new website", body: "Lorem ipsum...", author: "kat", id: 1 },
+    { title: "Welcome party", body: "Lorem ipsum...", author: "Thabo", id: 2 },
+    {
+      title: "Web dev top tips",
+      body: "Lorem ipsum...",
+      author: "Locus",
+      id: 3,
+    },
+  ]);
 
   return (
     <div className="home">
-      <h2>Homepage</h2>
-      <p>
-        {name} is {age} years old
-      </p>
-
-      <button onClick={handleClick}>Click me</button>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+          <p>{blog.body}</p>
+        </div>
+      ))}
     </div>
   );
 };
