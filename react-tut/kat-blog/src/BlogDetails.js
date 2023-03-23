63cee072b5f2c86e738bc866c076610061e1e1ac
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2023-03-23 20:24:34 +0200

    react tut kat-blog DELETE Requests

--- react-tut/kat-blog/src/BlogDetails.js
+++ react-tut/kat-blog/src/BlogDetails.js
@@ -1,11 +1,20 @@
-import { useParams } from "react-router";
+import { useHistory, useParams } from "react-router";
 import useFetch from "./useFetch";
 
 const BlogDetails = () => {
   const { id } = useParams();
+  const history = useHistory();
   const { data, error, isPending } = useFetch(
     "http://localhost:8000/blogs/" + id
   );
+
+  const handleDelete = () => {
+    fetch("http://localhost:8000/blogs/" + data.id, {
+      method: "DELETE",
+    }).then(() => {
+      history.push("/");
+    });
+  };
   return (
     <div className="blog-details">
       {isPending && <div>Loading...</div>}
@@ -15,6 +24,7 @@ const BlogDetails = () => {
           <h2>{data.title}</h2>
           <p>Written by {data.author}</p>
           <div>{data.body}</div>
+          <button onClick={handleDelete}>Delete</button>
         </article>
       )}
     </div>
