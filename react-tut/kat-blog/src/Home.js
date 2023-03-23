2f941c9dbb14ae240e43dbe2c58db8dbdff120c0
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2023-03-23 13:06:49 +0200

    react tut kat-blog creating a custom Hook

--- react-tut/kat-blog/src/Home.js
+++ react-tut/kat-blog/src/Home.js
@@ -1,34 +1,12 @@
-import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-  const [isPending, setIsPending] = useState(true);
-  const [error, setError] = useState(true);
-
-  // not store useEffect() to const cos it does not return anything but just pass as argument function
-  // this fuction fires in every render
-  useEffect(() => {
-    setTimeout(() => {
-      fetch("http://localhost:8000/blogss")
-        .then((response) => {
-          if (!response.ok) {
-            throw Error("could not fetch the data for that resource");
-          }
-          return response.json();
-        })
-        .then((data) => {
-          setBlogs(data);
-          setIsPending(false);
-        })
-        .catch((err) => {
-          setError(err.message);
-          setIsPending(false);
-        });
-    }, 1000);
-  }, []);
-  // [] dependency array makes sure useEffect() runs the function once after the initail render
-  // [name] ensures useEffect() runs the function again when name changes
+  const {
+    data: blogs,
+    isPending,
+    error,
+  } = useFetch("http://localhost:8000/blogs");
 
   return (
     <div className="home">
---
+++ react-tut/kat-blog/src/useFetch.js
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+
+// cutomer Hook in react  must alaways start with use..else it wont work
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
+
+  // not store useEffect() to const cos it does not return anything but just pass as argument function
+  // this fuction fires in every render
+  useEffect(() => {
+    setTimeout(() => {
+      fetch(url)
+        .then((response) => {
+          if (!response.ok) {
+            throw Error("could not fetch the data for that resource");
+          }
+          return response.json();
+        })
+        .then((data) => {
+          setData(data);
+          setIsPending(false);
+        })
+        .catch((err) => {
+          setError(err.message);
+          setIsPending(false);
+        });
+    }, 1000);
+  }, [url]);
+  // [] dependency array makes sure useEffect() runs the function once after the initail render
+  // [url] ensures useEffect() runs the function again when name changes
+
+  return { data, isPending, error };
+};
+
+export default useFetch;
