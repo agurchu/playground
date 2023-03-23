4188b69a3b4bdeefe88575dc04fc7672b6cbee7e
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2023-03-23 11:00:06 +0200

    react tut kat-blog conditional templates

--- react-tut/kat-blog/src/Home.js
+++ react-tut/kat-blog/src/Home.js
@@ -3,19 +3,26 @@ import BlogList from "./BlogList";
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
 
   // not store useEffect() to const cos it does not return anything but just pass as argument function
   // this fuction fires in every render
   useEffect(() => {
-    fetch("http://localhost:8000/blogs")
-      .then((response) => response.json())
-      .then((data) => setBlogs(data));
+    setTimeout(() => {
+      fetch("http://localhost:8000/blogs")
+        .then((response) => response.json())
+        .then((data) => {
+          setBlogs(data);
+          setIsPending(false);
+        });
+    }, 1000);
   }, []);
   // [] dependency array makes sure useEffect() runs the function once after the initail render
   // [name] ensures useEffect() runs the function again when name changes
 
   return (
     <div className="home">
+      {isPending && <div>Loading...</div>}
       {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
       {/**blogs variable may be any name. 
       titleAttr changes get updated to BlogList component
