199e7d426a0f0acc874773a751d4af779a2674f5
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2023-03-24 21:36:07 +0200

    react tut codewithmosh Lifting State Up

--- react-tut/codewithmosh/src/App.js
+++ react-tut/codewithmosh/src/App.js
@@ -3,15 +3,67 @@ import NavBar from "./components/navbar";
 import "./App.css";
 import Counters from "./components/counters";
 
-function App() {
-  return (
-    <React.Fragment>
-      <NavBar />
-      <main className="container">
-        <Counters />
-      </main>
-    </React.Fragment>
-  );
+class App extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 0 },
+      { id: 2, value: 1 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 },
+      { id: 5, value: 0 },
+    ],
+  };
+
+  handleDelete = (counterId) => {
+    const counters = this.state.counters.filter((c) => c.id !== counterId);
+    this.setState({ counters: counters });
+  };
+
+  handleReset = () => {
+    this.state.counters.value = 0;
+    const reset = this.state.counters.map((counter) => {
+      counter.value = 0;
+      return counter;
+    });
+    this.setState({ counters: reset });
+  };
+
+  handleIncrement = (product) => {
+    // this.setState({ value: product.value++ });
+    const products = [...this.state.counters];
+    const index = products.indexOf(product);
+    products[index] = { ...product };
+    products[index].value++;
+    this.setState({ counters: products });
+  };
+  handleDecrement = (product) => {
+    const products = [...this.state.counters];
+    const index = products.indexOf(product);
+    if (product.value === 0) {
+      return;
+    }
+    products[index] = { ...product };
+    products[index].value--;
+    this.setState({ counters: products });
+  };
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar
+          totalCounters={this.state.counters.filter((c) => c.value > 0).length}
+        />
+        <main className="container">
+          <Counters
+            counters={this.state.counters}
+            onReset={this.hamdleReset}
+            onIncrement={this.handleIncrement}
+            onDecrement={this.handleDecrement}
+            onDelete={this.handleDelete}
+          />
+        </main>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
--- react-tut/codewithmosh/src/components/counters.jsx
+++ react-tut/codewithmosh/src/components/counters.jsx
@@ -2,64 +2,21 @@ import Counter from "./counter";
 import React, { Component } from "react";
 
 class Counters extends Component {
-  state = {
-    counters: [
-      { id: 1, value: 0 },
-      { id: 2, value: 1 },
-      { id: 3, value: 0 },
-      { id: 4, value: 0 },
-      { id: 5, value: 0 },
-    ],
-  };
-
-  handleDelete = (counterId) => {
-    const counters = this.state.counters.filter((c) => c.id !== counterId);
-    this.setState({ counters: counters });
-  };
-
-  handleReset = () => {
-    this.state.counters.value = 0;
-    const reset = this.state.counters.map((counter) => {
-      counter.value = 0;
-      return counter;
-    });
-    this.setState({ counters: reset });
-  };
-
-  handleIncrement = (product) => {
-    // this.setState({ value: product.value++ });
-    const products = [...this.state.counters];
-    const index = products.indexOf(product);
-    products[index] = { ...product };
-    products[index].value++;
-    this.setState({ counters: products });
-  };
-  handleDecrement = (product) => {
-    const products = [...this.state.counters];
-    const index = products.indexOf(product);
-    if (product.value === 0) {
-      return;
-    }
-    products[index] = { ...product };
-    products[index].value--;
-    this.setState({ counters: products });
-  };
-
   render() {
     return (
       <div>
         <button
-          onClick={this.handleReset}
+          onClick={this.props.onReset}
           className="btn btn-primary btn-sm m-2"
         >
           Reset
         </button>
-        {this.state.counters.map((counter) => (
+        {this.props.counters.map((counter) => (
           <Counter
             key={counter.id}
-            onIncrement={this.handleIncrement}
-            onDecrement={this.handleDecrement}
-            onDelete={this.handleDelete}
+            onIncrement={this.props.onIncrement}
+            onDecrement={this.props.onDecrement}
+            onDelete={this.props.onDelete}
             counter={counter}
           />
         ))}
