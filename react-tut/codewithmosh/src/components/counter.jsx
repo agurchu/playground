3ee7fa36cad16b05653054ed64a72b2eacb5e8e7
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2023-03-24 13:45:34 +0200

    react tut codewithmosh Setting Attributes

--- react-tut/codewithmosh/src/components/counter.jsx
+++ react-tut/codewithmosh/src/components/counter.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  // any name
   state = {
     count: 0,
   };
@@ -8,8 +9,8 @@ class Counter extends Component {
   render() {
     return (
       <div>
-        <span>{this.formatCount()}</span>
-        <button>Increament</button>
+        <span className="badge badge-primary m-2">{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increament</button>
       </div>
     ); // this is jsx expression
   }
