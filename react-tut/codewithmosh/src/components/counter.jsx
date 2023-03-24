1135fd69755dee70bc563b4a51822e15af80eab8
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2023-03-24 15:39:55 +0200

    react tut codewithmosh handling events

--- react-tut/codewithmosh/src/components/counter.jsx
+++ react-tut/codewithmosh/src/components/counter.jsx
@@ -4,30 +4,38 @@ class Counter extends Component {
   // any name
   state = {
     count: 0,
-    tags: ["tag1", "tag2", "tag3"],
   };
 
-  renderTags() {
-    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
-
-    return (
-      <ul>
-        {this.state.tags.map((tag) => (
-          <li key={tag}>
-            {tag}
-          </li> /* whenever use map instert key to make the list unique */
-        ))}
-      </ul>
-    );
+  handleIncrement() {
+    console.log("increment clicked");
   }
 
   render() {
     return (
       <div>
-        {this.state.tags.length === 0 && <p>"Please create a new tag</p>}
-        {this.renderTags()}
+        <span className={`badge ${this.getBadgeClasses()} m-2`}>
+          {this.formatCount()}
+        </span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increament
+        </button>
       </div>
     ); // this is jsx expression
+  }
+
+  formatCount() {
+    // return this.state.count === 0 ? "Zero" : this.state.count;
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+    // return count === 0 ? <h1>Zero</h1> : count;
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    return (classes += this.state.count === 0 ? "warning" : "primary");
   }
 }
 
