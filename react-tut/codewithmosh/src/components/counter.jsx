6f3665b5a59c2ebe9780fde782aad496009c0fd0
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2023-03-24 14:03:09 +0200

    react tut codewithmosh renderinclasses dynamically

--- react-tut/codewithmosh/src/components/counter.jsx
+++ react-tut/codewithmosh/src/components/counter.jsx
@@ -3,13 +3,15 @@ import React, { Component } from "react";
 class Counter extends Component {
   // any name
   state = {
-    count: 0,
+    count: 1,
   };
 
   render() {
     return (
       <div>
-        <span className="badge badge-primary m-2">{this.formatCount()}</span>
+        <span className={`badge ${this.getBadgeClasses()} m-2`}>
+          {this.formatCount()}
+        </span>
         <button className="btn btn-secondary btn-sm">Increament</button>
       </div>
     ); // this is jsx expression
@@ -20,6 +22,11 @@ class Counter extends Component {
     const { count } = this.state;
     return count === 0 ? "Zero" : count;
     // return count === 0 ? <h1>Zero</h1> : count;
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    return (classes += this.state.count === 0 ? "warning" : "primary");
   }
 }
 
