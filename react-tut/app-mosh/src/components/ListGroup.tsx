189a41f1ef8914bc985c953fecfe906e3f0d9c6c
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2023-03-25 14:43:13 +0200

    react tut app-mosh Handling Events

--- react-tut/app-mosh/src/components/ListGroup.tsx
+++ react-tut/app-mosh/src/components/ListGroup.tsx
@@ -1,14 +1,18 @@
+import { MouseEvent } from "react";
+
 const ListGroup = () => {
   let items = ["Polokwane", "BGF", "Joburg", "Cape Town", "Durban"];
-  items = [];
+
+  // event Handler function
+  const handleClick = (event: MouseEvent) => console.log(event.);
 
   return (
     <>
       <h1>List</h1>
       {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item} className="list-group-item">
+        {items.map((item, index) => (
+          <li onClick={handleClick} key={item} className="list-group-item">
             {item}
           </li>
         ))}
