fad449b92824caa20dde4de63e762b3727b2f21a
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2023-03-25 18:42:09 +0200

    react tut app-mosh Passing Children

--- react-tut/app-mosh/src/App.tsx
+++ react-tut/app-mosh/src/App.tsx
@@ -1,28 +1,12 @@
-import { useState } from "react";
-
 import "./App.css";
-import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
 
 function App() {
-  let cities = ["Polokwane", "BGF", "Joburg", "Cape Town", "Durban"];
-  let countries = ["USA", "RSA", "Arg", "Bra", "Pug"];
-
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
-
   return (
     <div>
-      <ListGroup
-        items={cities}
-        heading="Cities"
-        onSelectItem={handleSelectItem}
-      />
-      <ListGroup
-        items={countries}
-        heading="Countries"
-        onSelectItem={handleSelectItem}
-      />
+      <Alert>
+        Hello <span>World</span>
+      </Alert>
     </div>
   );
 }
