77596c68c445b6c19996b2d362835354bf930fcf
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2023-03-25 19:34:08 +0200

    react tut app-mosh Showing an Akert Exercise

--- react-tut/app-mosh/src/App.tsx
+++ react-tut/app-mosh/src/App.tsx
@@ -1,11 +1,17 @@
+import { useState } from "react";
 import "./App.css";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
 
 function App() {
+  const [alertVisble, setAlertVisibility] = useState(false);
+
   return (
     <div>
-      <Button color="warning" onClick={() => console.log("clicked")}>
+      {alertVisble && (
+        <Alert onClick={() => setAlertVisibility(false)}>My Alert</Alert>
+      )}
+      <Button color="warning" onClick={() => setAlertVisibility(true)}>
         My Button
       </Button>
     </div>
--- react-tut/app-mosh/src/components/Alert.tsx
+++ react-tut/app-mosh/src/components/Alert.tsx
@@ -1,11 +1,17 @@
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
 
 interface Props {
   children: ReactNode;
+  onClick: () => void;
 }
 
-const Alert = ({ children }: Props) => {
-  return <div className="alert alert-primary">{children}</div>;
+const Alert = ({ children, onClick }: Props) => {
+  return (
+    <div className="alert alert-primary alert-dismissible">
+      {children}
+      <button onClick={onClick} className="btn-close"></button>
+    </div>
+  );
 };
 
 export default Alert;
