b94956393236276e493a511e76cfe1504521e3cc
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2023-04-13 20:19:17 +0200

    CREATING .ENV

--- react-tut/send/signupfront/signupbackend/index.js
+++ react-tut/send/signupfront/signupbackend/index.js
@@ -1,4 +1,11 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+dotenv.config();
+
+mongoose.connect(process.env.DATABASE_ACCESS);
+console.log(process.env.DATABASE_ACCESS);
 
 app.listen(8000, () => console.log("server is up and running"));
