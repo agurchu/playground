8a5363afafbdfa953de2368d905d4b4a2f305b0a
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2023-04-13 21:40:46 +0200

    trouble with api

--- react-tut/send/signupfront/signupbackend/index.js
+++ react-tut/send/signupfront/signupbackend/index.js
@@ -2,10 +2,14 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const routesUrls = require("./routes/routes");
+const cors = require("cors");
 
 dotenv.config();
 
 mongoose.connect(process.env.DATABASE_ACCESS);
-console.log(process.env.DATABASE_ACCESS);
 
+app.use(express.json());
+app.use(cors());
+app.use("/api", routesUrls);
 app.listen(8000, () => console.log("server is up and running"));
