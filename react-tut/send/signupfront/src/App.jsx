5c1390c8e352e06c89ff362be6841894a8249d62
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2023-04-15 10:21:30 +0200

    install bootstrap and initialize front vite

---
+++ react-tut/send/signupfront/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.css";
+
+function App() {
+  const [fullName, setFullName] = useState("");
+  const [userName, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return <div>app</div>;
+}
+
+export default App;
