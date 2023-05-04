5e0b646e58088190bf29aa3b15b866299c8f6cc2
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2023-05-04 08:55:55 +0200

    initialize http-app

---
+++ react-tut/http-app/src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.css";
+
+import "./App.css";
+
+function App() {
+  return <div className="container">hello</div>;
+}
+
+export default App;
