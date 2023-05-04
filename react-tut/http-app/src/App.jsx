73d75b837b3c0b2b9fedad1c8634396a2ff0cca0
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2023-05-04 17:45:42 +0200

    axios interceptors

--- react-tut/http-app/src/App.jsx
+++ react-tut/http-app/src/App.jsx
@@ -3,6 +3,18 @@ import "bootstrap/dist/css/bootstrap.css";
 import axios from "axios";
 
 const apiEndpoint = "https://jsonplaceholder.typicode.com/posts";
+axios.interceptors.response.use(null, (error) => {
+  const expectedError =
+    error.response &&
+    error.response.status >= 400 &&
+    error.response.status < 500;
+
+  if (!expectedError) {
+    console.log("logging the error", error);
+    alert("An unexpected error occurred");
+  }
+  return Promise.reject(error);
+});
 
 function App() {
   const [posts, setPosts] = useState([]);
@@ -34,17 +46,11 @@ function App() {
     setPosts(posts.filter((newPost) => newPost.id !== post.id));
     try {
       await axios.delete(apiEndpoint + "/" + post.id);
-      throw new Error("");
     } catch (error) {
       // Expected (404: not found, 400: bad request) - client errors
       // - display a specific error msg
       if (error.response && error.response.status === 404)
         alert("This post has already been deleted");
-      else {
-        console.log("logging the error", error);
-        alert("An unexpected error occurred");
-      }
-
       // Unexpected (network down, server down, db down, bug)
       // - log them
       // - display a generic and friendly error msg
